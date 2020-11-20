b1f1c1caed0e1a8633ac6ecc9dcc6e071624758e
Jungho Kwon <kwoner61@gmail.com>
2020-11-19 21:04:48 -0500

    WIP

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
+const port = 8080;
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+function listening(){
+     console.log("server running");
+     console.log(`running on localhost: {$port}`);
+}
 
 // Setup Server
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
