ff00d6c33bf5f3e69b12ba559f98c1da9c9e64f6
Jungho Kwon <kwoner61@gmail.com>
2020-12-24 02:49:27 -0600

    Evaluate News NLP: Stage 1 Complete

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,23 +1,26 @@
-var path = require('path')
-const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+const bodyParser = require('body-parser');
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
 const app = express()
 
-app.use(express.static('dist'))
+dotenv.config();
+console.log(`Your API key is ${process.env.API_KEY}`)
 
-console.log(__dirname)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    res.sendFile('dist/index.html')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Node app listening on port 8081!')
 })
--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -6,14 +6,22 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     devtool: 'source-map',
-    stats: 'verbose',
+    //stats: 'verbose',
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
