7486f9740c934da9dc159a1fd7528d6e94906fcb
Jungho Kwon <kwoner61@gmail.com>
2020-12-25 00:15:13 -0600

    Validate and reset user input message.

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -5,20 +5,29 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
-    //console.log("::: Form Submitted :::")
+    validateInputText(formText) ? 
+        sendSentimentRequest(formText) : alert('That\'s an invalid message.')
+}
+
+function sendSentimentRequest(formText) {
     fetch('http://localhost:8081/sentiment-analysis', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({
             'text': formText
         })
     })
-    .then(res => res.json())
-    .then(res => {
-        document.getElementById('results').innerHTML = res.score_tag
-    })    
+        .then(res => res.json())
+        .then(res => {
+            document.getElementById('results').innerHTML = res.score_tag
+            document.getElementById('name').value = ''
+        })
+}
+
+function validateInputText(inputText) {
+    return inputText.length > 0
 }
 
 export { handleSubmit }
